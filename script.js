84aa153743f3e777d8c2f4f6d7a68e8b0c71fadc
Santhakumar <121466055+2001-skd@users.noreply.github.com>
2025-04-27 20:24:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const search = document.getElementById("search");
+search.addEventListener("click", fetchUser);
+
+function fetchUser() {
+  let inputValue = document.getElementById("input_value").value;
+  let suggestion = document.getElementById("suggestion");
+  let profileCard = document.getElementById("profile_card");
+  let url = `https://api.github.com/users/${inputValue}`;
+  if (inputValue === "") {
+    suggestion.style.display = "block";
+  } else {
+    suggestion.style.display = "none";
+
+    // console.log(inputValue);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Not Found") {
+          //   console.log("User Not Found");
+          profileCard.innerHTML = `User Not Found`;
+          profileCard.style.display = "flex";
+        } else {
+          profileCard.innerHTML = `<div>
+          <img src=${data.avatar_url} />
+        </div>
+        <div class="profile_details">
+          <h4>${data.name}</h4>
+          <p>
+            ${data.bio}
+          </p>
+
+          <div class="profile_counts">
+            <div>
+              <p>${data.public_repos}</p>
+              <p>Repository</p>
+            </div>
+
+            <div>
+              <p>${data.followers}</p>
+              <p>Followers</p>
+            </div>
+
+            <div>
+              <p>${data.following}</p>
+              <p>Following</p>
+            </div>
+          </div>
+
+          <button class="profile_btn" id="profile_btn">
+            <a href=${data.html_url}>Github User Profile</a>
+          </button>
+        </div>`;
+
+          profileCard.style.display = "flex";
+          document.getElementById("input_value").value = "";
+        }
+      });
+  }
+}
